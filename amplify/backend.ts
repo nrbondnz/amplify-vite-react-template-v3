72a1a2dc30ddae1ea7bf9d8b902c7ce18b8ccbf6
Bond in Auckland <nigel.bond@orcon.net.nz>
2024-11-25 00:43:57 +1300

    wrds

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,16 +1,16 @@
 import { defineBackend } from '@aws-amplify/backend';
-//import { storage } from "./storage/resource";
-//import { storage } from "./storage/resource";
+import { storage } from "./storage/resource";
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
-const backend = defineBackend({
+defineBackend({
   auth,
-  data
+  data,
+  storage,
 });
 
-backend.addOutput({
+/*backend.addOutput({
   storage: {
     aws_region: "ap-southeast-2",
     bucket_name: "my-app-bucket007"
-  }});
+  }});*/
--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,11 +1,10 @@
 ﻿import { defineStorage } from '@aws-amplify/backend';
 
-/*
 export const storage = defineStorage({
 	name: 'amplifyTeamDrive',
 	access: (allow) => ({
-		'images/machines/!*': [
+		'images/machines/*': [
 			allow.entity('identity').to(['read', 'write', 'delete'])
 		]
 	})
-});*/
+});
